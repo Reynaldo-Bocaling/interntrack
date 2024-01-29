091fab1104709c59c7271fd375c664e0cc6c6265
Reynaldo Bocaling <reynladobocaling@gmail.com>
2024-01-29 13:10:15 +0800

    add cusmise on time logs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ const LandingPage = lazy(() => import("./pages/landingPage/index"));
 const PrivateRoutes = lazy(() => import("./auth/PrivateRoutes"));
 
 import { pdfjs } from "react-pdf";
-import List from "./components/print-layout/List";
+const List = lazy(()=> import("./components/print-layout/List"))
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -34,7 +34,7 @@ const App = () => {
     >
       <Routes>
         <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
-        <Route path="/" element={<PrivateRoutes />} />
+        <Route path="/*" element={<PrivateRoutes />} />
         <Route path="/weekly" element={<List />} />
       </Routes>
     </Suspense>
