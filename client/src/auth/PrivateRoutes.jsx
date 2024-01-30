9ce250fcd8cb3bd4618fc11d5c0cede1f796d7fe
Reynaldo Bocaling <reynladobocaling@gmail.com>
2024-01-30 18:26:07 +0800

    optimized

--- client/src/auth/PrivateRoutes.jsx
+++ client/src/auth/PrivateRoutes.jsx
@@ -181,19 +181,7 @@ const PrivateRoutes = () => {
 
   if (isLoading) {
     return (
-      <div className="fixed top-0 l-20 h-screen w-full bg-white flex mt-32 justify-center">
-        <div className="flex flex-col gap-4">
-          <PulseLoader
-            color="#03A8F5"
-            margin={8}
-            size={15}
-            speedMultiplier={1}
-          />
-          <span className="text-gray-400 text-2xl tracking-wider font-medium">
-            Loading..
-          </span>
-        </div>
-      </div>
+      <Spinner />
     );
   }
 
