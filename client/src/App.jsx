4b31f2ecc0797cd7d7d2797675bc7cb90f40cb99
Reynaldo Bocaling <reynladobocaling@gmail.com>
2024-01-30 17:57:54 +0800

    optimized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/landingPage/index";
-// const PrivateRoutes = lazy(() => import("./auth/PrivateRoutes"));
-import PrivateRoutes from "./auth/PrivateRoutes";
+import PulseLoader from "react-spinners/PulseLoader";
+const LandingPage = lazy(() => import("./pages/landingPage/index"));
+const PrivateRoutes = lazy(() => import("./auth/PrivateRoutes"));
+
 import { pdfjs } from "react-pdf";
 import AddAdminModal from "./components/addAdmin/AddAdminModal";
-import List from "./components/print-layout/List";
 import { Spinner } from "./components/spinners-loading/Spinner";
+const List = lazy(()=> import("./components/print-layout/List"))
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -15,20 +16,18 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 const App = () => {
   return (
-    <Routes>
-      <Route
-        path="/*"
-        element={
-         
-            <PrivateRoutes />
-       
-        }
-      />
-      <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
-      <Route path="/weekly" element={<List />} />
-      <Route path="/@super-admin" element={<AddAdminModal />} />
-    </Routes>
-    // </Suspense>
+    <Suspense
+      fallback={
+        <Spinner />
+      }
+    >
+      <Routes>
+        <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
+        <Route path="/*" element={<PrivateRoutes />} />
+        <Route path="/weekly" element={<List />} />
+        <Route path="/@super-admin" element={<AddAdminModal />} />
+      </Routes>
+    </Suspense>
   );
 };
 
