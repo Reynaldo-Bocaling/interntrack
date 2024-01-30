e38804914fb3bcc6ee0181ad98d5354dc85ace30
Reynaldo Bocaling <reynladobocaling@gmail.com>
2024-01-31 04:49:53 +0800

    optimized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-const LandingPage = lazy(() => import("./pages/landingPage/index"));
+import LandingPage from "./pages/landingPage/index";
 import PrivateRoutes from "./auth/PrivateRoutes";
 import { pdfjs } from "react-pdf";
-const AddAdminModal = lazy(() => import("./components/addAdmin/AddAdminModal"));
-const List = lazy(() => import("./components/print-layout/List"));
+import AddAdminModal from "./components/addAdmin/AddAdminModal";
+import List from "./components/print-layout/List"
 import PulseLoader from "react-spinners/PulseLoader";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -34,9 +34,12 @@ const App = () => {
       > */}
         <Routes>
           <Route path="/*" element={<PrivateRoutes />} />
+          <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
+          <Route path="/weekly" element={<List />} />
+          <Route path="/@super-admin" element={<AddAdminModal />} />
         </Routes>
       {/* </Suspense> */}
-      <Suspense
+      {/* <Suspense
         fallback={
           <div className="fixed top-0 l-20 h-screen w-full bg-white flex mt-32 justify-center">
             <div className="flex flex-col gap-4">
@@ -55,11 +58,9 @@ const App = () => {
         }
       >
         <Routes>
-          <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
-          <Route path="/weekly" element={<List />} />
-          <Route path="/@super-admin" element={<AddAdminModal />} />
+         
         </Routes>
-      </Suspense>
+      </Suspense> */}
     </>
   );
 };
