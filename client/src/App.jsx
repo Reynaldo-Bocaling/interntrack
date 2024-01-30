34d0f55d85096b2b964f820baccde4d032566794
Reynaldo Bocaling <reynladobocaling@gmail.com>
2024-01-30 14:01:34 +0800

    optimized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import PulseLoader from "react-spinners/PulseLoader";
-const LandingPage = lazy(() => import("./pages/landingPage/index"));
+import LandingPage from "./pages/landingPage/index";
 const PrivateRoutes = lazy(() => import("./auth/PrivateRoutes"));
+import PulseLoader from "react-spinners/PulseLoader";
 
 import { pdfjs } from "react-pdf";
 import AddAdminModal from "./components/addAdmin/AddAdminModal";
-const List = lazy(()=> import("./components/print-layout/List"))
+import Login from "./auth/Login";
+import List from "./components/print-layout/List";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -15,31 +16,38 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 const App = () => {
   return (
-    <Suspense
-      fallback={
-        <div className="fixed top-0 l-20 h-screen w-full bg-white flex mt-32 justify-center">
-          <div className="flex flex-col gap-4">
-            <PulseLoader
-              color="red"
-              margin={8}
-              size={15}
-              speedMultiplier={1}
-            />
+    <Routes>
+      <Route
+        path="/*"
+        element={
+          <Suspense
+            fallback={
+              <div className="fixed top-0 l-20 h-screen w-full bg-white flex mt-32 justify-center">
+                <div className="flex flex-col gap-4">
+                  <PulseLoader
+                    color="red"
+                    margin={8}
+                    size={15}
+                    speedMultiplier={1}
+                  />
 
-            <span className="text-gray-400 text-2xl tracking-wider font-medium">
-              Loading..
-            </span>
-          </div>
-        </div>
-      }
-    >
-      <Routes>
-        <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
-        <Route path="/*" element={<PrivateRoutes />} />
-        <Route path="/weekly" element={<List />} />
-        <Route path="/@super-admin" element={<AddAdminModal />} />
-      </Routes>
-    </Suspense>
+                  <span className="text-gray-400 text-2xl tracking-wider font-medium">
+                    Loading..
+                  </span>
+                </div>
+              </div>
+            }
+          >
+            <PrivateRoutes />
+          </Suspense>
+        }
+      />
+      <Route path="/welcome-to-InternTrack" element={<LandingPage />} />
+      <Route path="/weekly" element={<List />} />
+      <Route path="/@super-admin" element={<AddAdminModal />} />
+      <Route path="/auth" element={<Login />} />
+    </Routes>
+    // </Suspense>
   );
 };
 
