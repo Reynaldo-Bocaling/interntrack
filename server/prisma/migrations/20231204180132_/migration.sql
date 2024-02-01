77ed313fe3835ffa75ef00bf117cabf401cb24b2
Reynaldo Bocaling <reynladobocaling@gmail.com>
2024-02-02 06:17:41 +0800

    optimized

--- server/prisma/migrations/20231204180132_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `Announcement` ADD CONSTRAINT `Announcement_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `Student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
